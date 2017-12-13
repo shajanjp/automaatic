28eab533da018e208654e51a3be587a3b0f7b230
Shajan Jacob <shajanjp@gmail.com>
2017-12-13 18:47:13 +0530

    initial commmit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyChLMgEA8LR8DgpCBtzx_PuLGFWO-DsXko",
+    authDomain: "autooomatic.firebaseapp.com",
+    databaseURL: "https://autooomatic.firebaseio.com",
+    projectId: "autooomatic",
+    storageBucket: "autooomatic.appspot.com",
+    messagingSenderId: "450514363172"
+  };
+  firebase.initializeApp(config);
